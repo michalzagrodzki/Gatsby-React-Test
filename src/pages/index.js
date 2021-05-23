4ec7de714f2f8c9f9f659ff365697bc51f223685
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-05-23 18:17:31 +0200

    Update template

--- src/pages/index.js
+++ src/pages/index.js
@@ -152,20 +152,13 @@ const IndexPage = () => {
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Welcome
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>— this is my first Gatsby site. </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
       </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
