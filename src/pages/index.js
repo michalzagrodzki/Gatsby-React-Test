f8431138cde73eb6c201c7b2902c0d5eb47a2e38
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-05-23 19:38:11 +0200

    Move styles to css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,82 +1,6 @@
 import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import { Link } from "gatsby"
+import { container, listItemStyles, linkStyle, badgeStyle, descriptionStyle, headingStyles, headingAccentStyles, listStyles, docLinkStyle} from "./about.module.css"
 
 // data
 const links = [
@@ -127,20 +51,20 @@ const links = [
 function LinksList(props) {
   const links = props.links;
   return (links.map(link => 
-    <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+    <li key={link.url} className={listItemStyles}>
       <span>
         <a
-          style={linkStyle}
+          className={linkStyle}
           href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
         >
           {link.text}
         </a>
         {link.badge && (
-          <span style={badgeStyle} aria-label="New Badge">
+          <span className={badgeStyle} aria-label="New Badge">
             NEW!
           </span>
         )}
-        <p style={descriptionStyle}>{link.description}</p>
+        <p className={descriptionStyle}>{link.description}</p>
       </span>
     </li>
   ));
@@ -149,25 +73,20 @@ function LinksList(props) {
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main className={container}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
+      <h1 className={headingStyles}>
         Welcome
         <br />
-        <span style={headingAccentStyles}>— this is my first Gatsby site. </span>
+        <span className={headingAccentStyles}>— this is my first Gatsby site. </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
       </h1>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
+      <ul className={listStyles}>
+        <li className={docLinkStyle}>
+          <Link className={linkStyle} to="/about">About</Link>
+        </li> 
         <LinksList links={links} />
       </ul>
       <img
