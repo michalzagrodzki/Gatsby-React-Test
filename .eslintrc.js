db72650bca815bb8ca26e37371857ae6fa777cc1
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-05-29 21:45:42 +0200

    Update rules for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,25 +2,36 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'prettier'
-  ],
+  extends: ["plugin:react/recommended", "eslint:recommended", "prettier"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
     "eol-last": 1,
-    "max-len": 1
-  }
-}
+    "max-len": 1,
+    curly: 1,
+    "no-empty-function": 1,
+    "no-else-return": 1,
+    "no-use-before-define": 1,
+    "array-bracket-spacing": [1, "never"],
+    "block-spacing": 1,
+    camelcase: 1,
+    indent: [1, 2],
+    "jsx-quotes": [1, "prefer-double"],
+    "keyword-spacing": ["error", { before: true, after: true }],
+    "max-lines": [1, { max: 150, skipBlankLines: true, skipComments: true }],
+    "max-lines-per-function": ["error", { max: 40, skipComments: true }],
+    "no-inline-comments": 1,
+    "no-multiple-empty-lines": 1,
+    "no-trailing-spaces": 1,
+    "no-whitespace-before-property": 1,
+    "object-curly-spacing": [1, "always"],
+  },
+};
